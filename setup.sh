2b031ade464c37084b3f7eb86b29e30f9690bcbd
Rafael Corsi <corsiferrao@gmail.com>
2024-02-06 10:59:01 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ python -m pip install --upgrade pip
 pip install pytest toml
 wget -q -O /usr/local/bin/wokwi-cli https://github.com/rafaelcorsi/wokwi-cli/releases/latest/download/wokwi-cli-linuxstatic-x64
 chmod +x /usr/local/bin/wokwi-cli
+
+WOKWI_CLI_TOKEN="${{ secrets.WOKWI_CLI_TOKEN }}"
+echo "WOKWI_CLI_TOKEN=${WOKWI_CLI_TOKEN}" >> $GITHUB_ENV
+echo $WOKWI_CLI_TOKEN
