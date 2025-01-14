2037e69571b1ca6388da54a3d92040a7692dc6c7
danielmpinto <danielmessias.engenharia@gmail.com>
2025-01-14 17:33:18 -0300

    solving

---
+++ open-container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CURRENT=`pwd`
+BASENAME=`basename "$CURRENT"`
+
+
+ git submodule add git@github.com:insper-embarcados/container-essentials.git container-essentials-$BASENAME} || git pull --recurse-submodules && code container-essentials-$BASENAME
