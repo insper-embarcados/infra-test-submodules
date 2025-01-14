629cc0c1afe7d2639fb0ba4bb4fe03e49be0874b
danielmpinto <danielmessias.engenharia@gmail.com>
2025-01-14 17:34:46 -0300

    removing detail

--- open-container.sh
+++ open-container.sh
@@ -4,4 +4,4 @@ CURRENT=`pwd`
 BASENAME=`basename "$CURRENT"`
 
 
- git submodule add git@github.com:insper-embarcados/container-essentials.git container-essentials-$BASENAME} || git pull --recurse-submodules && code container-essentials-$BASENAME
+ git submodule add git@github.com:insper-embarcados/container-essentials.git container-essentials-$BASENAME || git pull --recurse-submodules && code container-essentials-$BASENAME
